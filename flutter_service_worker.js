459713740f8f04de77ec46951b72605f41cdf12b
Manna Kumari <mannakumari1873@gmail.com>
2025-09-20 12:26:48 +0530

    Built web
    Branch: main
    Commit: f5f1220e3456e5f31e1e283887d80656dcbd1c63 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,14 +37,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "120d3fe3ff97044a79e4356b1a6915b3
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "525c3bc25a48e346011d424509efda1b",
+"flutter_bootstrap.js": "7ffc46f491c8d1552554ddf85c78bf61",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "53a3cd6c25b2e7bcd7c5c0bbc2f8652a",
 "/": "53a3cd6c25b2e7bcd7c5c0bbc2f8652a",
-"main.dart.js": "31491851612c26df2b16c29a80cc08b8",
+"main.dart.js": "ab9d44f6f67850d8dc3c88ebaa213312",
 "manifest.json": "0e2dcbed2a8d25fab9e508b30f54031a",
 "version.json": "c73456e015eb0ef746a8df4a0a151f66"};
 // The application shell files that are downloaded before a service worker can
